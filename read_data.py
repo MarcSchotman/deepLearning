09668787d4a749aaf399af4da44d60614aa76a09
TaekedeHaan <taeke.dehaan@live.nl>
2018-05-08 11:22:49 +0200

    Working on deleting staions which have not been active all the years

--- read_data.py
+++ read_data.py
@@ -1,15 +1,20 @@
+def delete_indexes(index, my_list):
+    for index in sorted(index, reverse=True):
+        del my_list[index]
+    return my_list
+
 import ftplib
 import re # for stripping name (#curcentStationName = curcentStationName.strip('-2017.gz'))
 
 # loop over these years
 yearStart = 2012
-yearEnd = 2013
+yearEnd = 2016
 years = list(range(yearStart, yearEnd + 1))
 keys = ['USAF', 'WBAN'] # for station list
 
 # init
-stationYear = {}
-stationYear = stationYear.fromkeys(years)
+stations = {}
+stations = stations.fromkeys(years)
 # stationID = {}
 
 #Open ftp connection
@@ -22,7 +27,7 @@ ftp.cwd('pub/data/noaa/isd-lite/')
 parent_dir = ftp.pwd()
 
 for year in years:
-    stationYear[year] = [] # init dict as list
+    stations[year] = [] # init dict as list
     
     # reset dict
 #    stationID = stationID.fromkeys(keys)
@@ -32,8 +37,6 @@ for year in years:
     
     print('Extracting year data from ' + str(year) + '.')
     ftp.cwd(str(year)) # go to year dict
-    
-
     
     
     #List the files in the current directory
@@ -50,12 +53,12 @@ for year in years:
         curcentStationID = re.sub('-' + str(year) +'.gz', '', curcentStationID)
         
         # split at -
-        curcentStationID = curcentStationID.split('-')
+        # curcentStationID = curcentStationID.split('-')
         
         # map to int
-        curcentStationID = list(map(int, curcentStationID))
+        #vcurcentStationID = list(map(int, curcentStationID))
          
-        stationYear[year].append(curcentStationID)
+        stations[year].append(curcentStationID)
         
         # add station ID to dict
         # stationID[keys[0]].append(curcentStationID[0])
@@ -66,27 +69,43 @@ for year in years:
     # Go to parent dictionary
     ftp.cwd('..')
     
-# example
-print(stationYear[year])
 
 # Check which station was active in each year
-activeEachYear = stationYear[2012]
+activeEachYearInit = stations[yearStart]
 
+activeEachYear = activeEachYearInit
+delCount = 0
+# delete staions which are not active each year
 for year in years:
-    for stationID in stationYear[year]:
+    print('==Start year==')
+    print(year)
+    
+    # reset
+    delIndex = []
+    
+    # loop over active each year
+    for i in range(0, len(activeEachYear)):
         
-        if contains(activeEachYear, stationID)
+        if not (activeEachYear[i] in stations[year]):
+            delIndex.append(i)
+            delCount = delCount + 1
+            print(year)
+            
+    activeEachYear = delete_indexes(delIndex, activeEachYear)
 
-stationYear2012 = stationYear[2012]
-stat
-result = cmp(stationYear[2012], stationYear[2012])
+
+# test delete indexes            
+# delete_indexes([1], [1, 2, 3])
 
 
-shared_items = set(stationYear2012.items()) & set(stationYear2012.items())
-print(shared_items)
+print(activeEachYear)
+
+print(len(stations[2012]))
+print(len(stations[2012]) - delCount)
+print(len(activeEachYear))
+
 
 ftp.quit()
-
 # cmp(dict1, dict2)
 # =============================================================================
 # #Get the readme file
