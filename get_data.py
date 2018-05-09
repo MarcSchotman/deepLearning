40cc1342ffbeb14e327c7e6f7c0903ae7328d45c
SchotmanG <m_a_schotman@hotmail.com>
2018-05-09 11:52:01 +0200

    get_data.py now works per year instead of on list of years for efficiency. main_data_mining now checks for previously downloaded data, also retries after an error.

--- get_data.py
+++ get_data.py
@@ -2,11 +2,11 @@
 """
 Created on Mon May  7 10:57:16 2018
 
-SUMMARY: SAVES ALL DATA LISTED IN KEYS FOR EACH WEATHER STATION IN THE LIST FOR EACH EYAR IN THE LIST PER YEAR USING PIKCLE
+SUMMARY: SAVES ALL DATA LISTED IN KEYS FOR EACH WEATHER STATION IN THE LIST FOR GIVEN YEAUSING PICKLE
 
 @author: m_a_s
 """
-def get_data(YEARS,USAF_ID, WBAN_ID, keys,destinationPath):
+def get_data(year,USAF_ID, WBAN_ID, keys,destinationPath):
     import ftplib
     import io
     import gzip
@@ -18,100 +18,80 @@ def get_data(YEARS,USAF_ID, WBAN_ID, keys,destinationPath):
     
     #concatenates the two ID's with '-' in between
     STATION_ID_LIST=["{}-{:2}".format(a_, b_) for a_, b_ in zip(USAF_ID, WBAN_ID)]
-    
-    #calculate total amount of years to keep track of progress
-    total_years=0
-    for years in YEARS:
-        total_years+=1
         
-    with ftplib.FTP(host=ftp_host) as ftpconn:
+    with ftplib.FTP(host=ftp_host, timeout = 600) as ftpconn:
         ftpconn.login()
-        counting=0
-        for year in YEARS:
-            percentage_done = counting/(total_years*100)
+        counting =0
+        #initilize dict for each year        
+        data_year={}
+        print('Downloading year ', year)
+        for station_id in STATION_ID_LIST:
+            ftp_file = "pub/data/noaa/{YEAR}/{ID}-{YEAR}.gz".format(ID=station_id, YEAR=year)
+            
+            #show file download and percentage of completention
             counting+=1
-            print("Downloading and processing files for year ", year,round(percentage_done,1),"% of total completed....")
+            percentage_done = counting/len(STATION_ID_LIST)*100
+            if int(len(STATION_ID_LIST)/10) ==0:
+                dummy_number = 1
+            else:
+                dummy_number =  int(len(STATION_ID_LIST)/10)
             
-            counting_inter =0
-            #initilize dict for each year        
-            data_year={}
-            for station_id in STATION_ID_LIST:
-                ftp_file = "pub/data/noaa/{YEAR}/{ID}-{YEAR}.gz".format(ID=station_id, YEAR=year)
-                
-                #show file download and percentage of completention
-                counting_inter+=1
-                percentage_done = counting_inter/len(STATION_ID_LIST)*100
-                if int(len(STATION_ID_LIST)/10) ==0:
-                    dummy_number = 1
+            if counting%dummy_number ==0:
+                print(round(percentage_done,1),"% completed....")
+    
+            
+            # read the whole file and save it to a BytesIO (stream)
+            response = io.BytesIO()
+            try:
+                ftpconn.retrbinary('RETR '+ftp_file, response.write)
+            except ftplib.error_perm as err:
+                if str(err).startswith('550 '):
+                    print('ERROR:', err)
                 else:
-                    dummy_number =  int(len(STATION_ID_LIST)/10)
+                    raise
+    
+            # decompress and parse each line 
+            response.seek(0) # jump back to the beginning of the stream
+            with gzip.open(response, mode='rb') as gzstream:
                 
-                if counting_inter%dummy_number ==0:
-                    print("Year ", year, " ", round(percentage_done,1),"% completed....")
-        
+                content =bytes.decode(gzstream.read())
+                gzstream.close()
                 
-                # read the whole file and save it to a BytesIO (stream)
-                response = io.BytesIO()
-                try:
-                    ftpconn.retrbinary('RETR '+ftp_file, response.write)
-                except ftplib.error_perm as err:
-                    if str(err).startswith('550 '):
-                        print('ERROR:', err)
+            #function from github which parses the nasty noaa data nicely for us.
+            parser.loads(content)                
+            reports = parser.get_reports()  
+            
+            #initilize nested dict for current station
+            data_year[station_id] ={}
+            #shows all possible keys (just so we remeber, dont delete please(!))
+            possible_keys = dir(reports[0])
+            
+            #initilizes keys inside nested dictionary
+            data_year[station_id] = data_year[station_id].fromkeys(keys)
+            #initilize keys as lists so they can fit the year long features
+            for i in keys:
+                data_year[station_id][i] = []
+            
+            #append the features in the appropraite dictionary
+            #slowest process in here: big nested loop.. loop1~6k, loop2=len(keys)
+            for report in reports:
+                for key in keys:
+                    #get numerical data from the parsed matrix
+                    value = getattr(report,key)
+                    if key == 'humidity':
+                        value_inserted = value.humidity
                     else:
-                        raise
-        
-                # decompress and parse each line 
-                response.seek(0) # jump back to the beginning of the stream
-                with gzip.open(response, mode='rb') as gzstream:
+                        try:
+                            value_inserted = value._obs_value
+                        except AttributeError:
+                            
+                            value_inserted = value
                     
-                    content =bytes.decode(gzstream.read())
-                    gzstream.close()
-                    
-                #function from github which parses the nasty noaa data nicely for us.
-                parser.loads(content)                
-                reports = parser.get_reports()  
-                
-                #initilize nested dict for current station
-                data_year[station_id] ={}
-                #shows all possible keys (just so we remeber, dont delete please(!))
-                possible_keys = dir(reports[0])
-                
-                #initilizes keys inside nested dictionary
-                data_year[station_id] = data_year[station_id].fromkeys(keys)
-                #initilize keys as lists so they can fit the year long features
-                for i in keys:
-                    data_year[station_id][i] = []
-                
-                #append the features in the appropraite dictionary
-                #slowest process in here: big nested loop.. loop1~6k, loop2=len(keys)
-                for report in reports:
-                    for key in keys:
-                        #get numerical data from the parsed matrix
-                        value = getattr(report,key)
-                        if key == 'humidity':
-                            value_inserted = value.humidity
-                        else:
-                            try:
-                                value_inserted = value._obs_value
-                            except AttributeError:
-                                
-                                value_inserted = value
-                        
-                        data_year[station_id][key].append(value_inserted)
-                        #getattr(report,i) makes it: report.i using the variable i.
-                        #report['air_temperature'] for example gives the air temperature of this line
-            
-            #time to save the dict
-            file_name = str(year)
-            full_path_name = os.path.join(destinationPath,file_name)
-            
-            if not os.path.exists(destinationPath):
-                    os.makedirs(destinationPath)
-                    
-            #uses pikcle for dumping dictionary
-            with open(full_path_name + '.pickle', 'wb') as handle:
-                pickle.dump(data_year, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        print("Finished downloads and processing of all files.")
+                    data_year[station_id][key].append(value_inserted)
+                    #getattr(report,i) makes it: report.i using the variable i.
+                    #report['air_temperature'] for example gives the air temperature of this line
+        return data_year
+
 
 
 
--- main_data_mining.py
+++ main_data_mining.py
@@ -5,7 +5,6 @@ Created on Mon May  7 22:02:59 2018
 @author: m_a_s
 """
 
-
 import numpy as np
 import os.path
 import pickle
@@ -16,20 +15,20 @@ from get_data import get_data
 #lattitude and longitude Delft
 lattitudeCenter = 52.0116
 longitudeCenter = 4.3571
-startYear = '2017'
-endYear = '2018'
-
+startYear = '2010'
+endYear = '2011'
 minStartDate = int(startYear+'0000') #YearMonthDay
 minEndDate = int(endYear+'0000')  #YearMonthDay
+map_location = 'D:\\DATA_NEW'
 
-r_list = [50]
+#DATA GATEHRED: 
+keys = ['datetime','air_temperature','humidity','elevation','dew-point','wind_speed','wind_direction','wind_observation_direction_type']
+    
+r_list = [50, 100, 200, 400,800]
 
 for r in r_list:
+    print('RADIUS: ', r, ' KM')
     stations = filter_stations(minStartDate, minEndDate, r, longitudeCenter, lattitudeCenter)
-    
-    
-    #DATA GATEHRED: 
-    keys = ['datetime','air_temperature','humidity','elevation','dew-point']#,'wind_speed','wind_direction','wind_observation_direction_type'
         
     # identifies what data to get
     YEARS = range(int(startYear), int(endYear))
@@ -39,7 +38,8 @@ for r in r_list:
     STATION_ID = [USAF_ID, WBAN_ID]
     
     mapName = 'RADIUS' + str(r) +'KM'
-    destinationPath = os.path.join('C:\\Users\\m_a_s\\Documents\\DUMMYDATA',mapName)
+    destinationPath = os.path.join(map_location,mapName)
+    
     if not os.path.exists(destinationPath):
         os.makedirs(destinationPath)
         
@@ -47,5 +47,65 @@ for r in r_list:
     with open(destinationPath +'\\STATION_ID' + '.pickle', 'wb') as handle:
         pickle.dump(STATION_ID, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
+    #check which stations have already been downloaded previously
+    current_index_r = r_list.index(r)
+    if current_index_r != 0:
+        prev_mapName='RADIUS' +str(r_list[current_index_r -1]) + 'KM'
+        previous_path = os.path.join(map_location,prev_mapName)        
+        
+        with open(previous_path+'\\STATION_ID' + '.pickle','rb') as handle:
+            prev_STATION_ID_LIST = pickle.load(handle)
+            prev_USAF = prev_STATION_ID_LIST[0]
+            prev_WBAN = prev_STATION_ID_LIST[1]
+            
+            print(prev_STATION_ID_LIST)
+    
+        #remove the already downloaded ID's from the ' to download list'      
+        removed_stations = []
+        for index in range(0,len(prev_USAF)):
+            
+            if prev_USAF[index] in USAF_ID:
+                index_match = USAF_ID.index(prev_USAF[index])
+                #if at the same idnex also the other ID matches THEN remove them from the list
+                if prev_WBAN[index] == WBAN_ID[index_match]:
+                    USAF_ID.remove(prev_USAF[index])
+                    WBAN_ID.remove(prev_WBAN[index])
+                    removed_stations.append([prev_USAF[index], prev_WBAN[index]])
+                    
     #downloads all data for all stations in station_ID and years in year
-    get_data(YEARS, USAF_ID,WBAN_ID,keys,destinationPath)
+    for year in YEARS:
+        try:
+            data_year = get_data(year, USAF_ID,WBAN_ID,keys,destinationPath)
+        except Exception as ex:
+            print('ERROR:', ex)
+            print('trying again...')
+            try:
+                data_year = get_data(year, USAF_ID,WBAN_ID,keys,destinationPath)
+            except Exception as ex:
+                print('ERROR: ',ex)
+                print('STOPPING DOWNLOAD.')
+        
+        #add previously downloaded data to the current data_year
+        if current_index_r != 0:        
+            with open(previous_path+'\\' + str(year)+ '.pickle','rb') as handle:
+                prev_data_year = pickle.load(handle)
+                
+            for index in range(0,len(removed_stations)):
+                station_id = removed_stations[index][0] + '-' + removed_stations[index][1]
+                data_year[station_id] = []
+                data_year[station_id] = prev_data_year[station_id].copy()
+            
+        
+        #time to save the dict
+        file_name = str(year)
+        full_path_name = os.path.join(destinationPath,file_name)
+        
+        if not os.path.exists(destinationPath):
+                os.makedirs(destinationPath)
+                
+        #uses pikcle for dumping dictionary
+        with open(full_path_name + '.pickle', 'wb') as handle:
+            pickle.dump(data_year, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        print("Finished YEAR ", year)
+        
+        
