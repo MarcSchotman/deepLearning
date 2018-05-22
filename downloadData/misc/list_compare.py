b7475c88e1164db00db916c1c6cc766cb29bec6c
TaekedeHaan <taeke.dehaan@live.nl>
2018-05-22 13:11:36 +0200

    Debugging filter stations

---
+++ downloadData/misc/list_compare.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 12:50:05 2018
+
+@author: Taeke
+"""
+
+a = [['1', '2'], ['3', '4']]
+
+b = ['1', '0']
+
+if (b in a):
+    print('Yes')
+    
+for ID in a:
+    print(ID)
