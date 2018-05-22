22deb6ee6ea532ecdd3f9f83c9050c4ebd2e64fc
SchotmanG <m_a_schotman@hotmail.com>
2018-05-22 10:33:30 +0200

    Fix temeprature bug.

--- pre_processing.py
+++ pre_processing.py
@@ -5,43 +5,66 @@ Created on Tue May 15 09:37:28 2018
 @author: Taeke
 """
 
+import datetime
 
 #INPUTS
 startYear = 2017
 endYear = 2018 #downloads UPTILL endyear so NOT 2018
 RADIUS = '100' #ASSSUMES DATA IN /deepLearning/data
 cut_off_percentage = 3 #ommits station with missing data > 3%
+maxDiff = datetime.timedelta(.5) #Maximum difference between matched dates
+
 import numpy as np
 import pickle
 import os.path
-import datetime
+
 import pytz
 from find_stations_unusable_temperatures import find_stations_unusable_temperatures
 import time
 
 
 
-def find_matched_indexes(dateList, datesStation):
+def match_dates(dateList, data_year, currentKeys, ID, maxDiff, missingValue):
+    #checks dates and fills in missing value if date difference is larger then  maxDiff
+    
     index = 1 #NEEDS TO BE 1
-    indexMatches = []
+    datesStation = data_year[ID]['datetime']
+    outList = {}
+    outList = dict.fromkeys(currentKeys)
+    for key in currentKeys:
+        outList[key] = []
+    
     
     for date in dateList:
         match = False
         last_delta = abs(date - datesStation[index-1])
+        
         while match==False:
             delta = abs(date - datesStation[index])
             if delta > last_delta:
-                indexMatches.append(index-1)
                 match =True
                 #makes sure that when at the last index it stays there i.e. not index+=1
             elif (index) == len(datesStation)-1:
-                indexMatches.append(index)
                 match =True
             else:
                 #only continue if there was no match
                 index +=1    
                 last_delta = delta
-    return indexMatches
+        
+        if last_delta > maxDiff:
+            missing = True
+        else:
+            missing = False
+            
+        for key in currentKeys:
+            if key == 'datetime':
+                outList[key] = dateList
+            elif missing:
+                outList[key].append(missingValue)
+            else:                    
+                outList[key].append(data_year[ID][key][index])
+
+    return outList
         
 def get_number_of_hours_year(year): 
     if (year % 4) == 0 and (year % 100 != 0 or year % 400 == 0):
@@ -77,6 +100,17 @@ for year in YEARS:
     # load data current year
     file = open(dataLocation, 'rb')
     data_year = pickle.load(file)
+    
+    #first check dates then check misding percentage
+    #create range of dates
+    base = datetime.datetime(year, 1, 1, 0, 0, tzinfo = pytz.UTC)
+    numHours = get_number_of_hours_year(year)
+    dateList = [base + datetime.timedelta(hours=x) for x in range(0, numHours)]      
+    
+    for ID in currentUsableStations:
+        currentKeys = list(data_year[ID].keys())
+        data_year[ID] = match_dates(dateList, data_year, currentKeys, ID, maxDiff, missingValue)
+        
     unusableStations = find_stations_unusable_temperatures(data_year, currentUsableStations, cut_off_percentage)
     
     for ID in unusableStations:
@@ -109,26 +143,14 @@ for year in YEARS:
     
     #loop over stations
     for ID in usableStations:
-        # print('Processing station:', ID,'...')
-        
         # get keys from data
         currentKeys = list(data_year[ID].keys())
         data_processed[ID] = dict.fromkeys(currentKeys, [])
         #get datetime list from station
-        datesMeasurement = data_year[ID]['datetime']
   
         tic = time.clock()
-        indexMatches = find_matched_indexes(dateList,datesMeasurement)
+        data_processed[ID]= match_dates(dateList, data_year, currentKeys, ID, maxDiff, missingValue)
         toc = time.clock()
-        # print(round(toc-tic,2),'s: matched dates (', len(datesMeasurement),' in list)')
-
-        #store in new dictionary
-        for key in currentKeys:
-            if key == 'datetime':
-                data_processed[ID][key] = dateList
-            else:
-                data_processed[ID][key] = [ data_year[ID][key][index] for index in indexMatches ]
-
         #have to assign dummy value otherwise deleted key gets printed
     #SAVE DICTIONARIES
     if not os.path.exists(processedFilesLocation):
