af007f5ed4322f8b1f984e8c52e9e7fefcae9808
SchotmanG <m_a_schotman@hotmail.com>
2018-05-29 11:44:52 +0200

    forgot this file

--- downloadData/functions/make_missingValueDict.py
+++ downloadData/functions/make_missingValueDict.py
@@ -6,9 +6,9 @@ Created on Tue May 29 11:01:24 2018
 """
 
 def make_missingValueDict():
-    filterKeys = ['air_temperature','sea_level_pressure','humidity','dew-point','wind_speed','wind_direction','wind_observation_direction_type']
+    allFilterableKeys = ['air_temperature','sea_level_pressure','humidity','dew-point','wind_speed','wind_direction','wind_observation_direction_type']
     missingValueList ={}
-    missingValueList = dict.fromkeys(filterKeys)
+    missingValueList = dict.fromkeys(allFilterableKeys)
     missingValueList['air_temperature'] = 999.9
     missingValueList['sea_level_pressure'] = 9999.9 
     missingValueList['humidity'] = 'MISSING'
@@ -17,4 +17,4 @@ def make_missingValueDict():
     missingValueList['dew_point'] =  999.9 #<---this is random... NO VALUES IN DEWPOINT......
     missingValueList['wind_observation'] = '999'
     missingValueList['wind_observation_direction_type'] =  ''
-    return missingValueList, filterKeys
+    return missingValueList
